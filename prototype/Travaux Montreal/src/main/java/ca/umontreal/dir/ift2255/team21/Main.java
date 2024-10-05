b0bfc9fafbbf8d1f291e3f06056640993808304d
Arpad <arpadbotond.rigo@outlook.com>
2024-10-05 18:28:11 -0400

    connection

--- prototype/Travaux Montreal/src/main/java/ca/umontreal/dir/ift2255/team21/Main.java
+++ prototype/Travaux Montreal/src/main/java/ca/umontreal/dir/ift2255/team21/Main.java
@@ -1,11 +1,16 @@
 package ca.umontreal.dir.ift2255.team21;
 
+import ca.umontreal.dir.ift2255.team21.cli.Display;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-
-
+        Display display = new Display();
+        int choiceIntro = display.introScreen();
+        System.out.println(choiceIntro);
+        if (choiceIntro == 1){
+        }
 
     }
 }
--- prototype/Travaux Montreal/src/main/java/ca/umontreal/dir/ift2255/team21/cli/Display.java
+++ prototype/Travaux Montreal/src/main/java/ca/umontreal/dir/ift2255/team21/cli/Display.java
@@ -1,4 +1,57 @@
 package ca.umontreal.dir.ift2255.team21.cli;
-
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class Display {
+    public int introScreen(){
+        Scanner choice = new Scanner(System.in);
+        int menuChoice = 0;
+
+        System.out.println("""
+                *********************************
+                **                             **
+                **   $$\\      $$\\ $$$$$$$$\\    **
+                **   $$$\\    $$$ |\\__$$  __|   **
+                **   $$$$\\  $$$$ |   $$ |      **
+                **   $$\\$$\\$$ $$ |   $$ |      **
+                **   $$ \\$$$  $$ |   $$ |      **
+                **   $$ |\\$  /$$ |   $$ |      **
+                **   $$ | \\_/ $$ |   $$ |      **
+                **   \\__|     \\__|   \\__|      **
+                **                             **
+                **    Bienvenue sur le site    **
+                **     Travaux de Montreal     **
+                **                             **
+                *********************************
+                **                             **
+                **    Choisissez un option:    **
+                **      1) Se Connecter        **
+                **      2)  S'inscrire         **
+                **                             **
+                *********************************
+                """);
+        System.out.print("Entrez le numéro de votre choix: ");
+        menuChoice = choice.nextInt();
+        choice.nextLine();
+        if (menuChoice != 1 && menuChoice != 2) {
+            menuChoice = introScreen();
+        }
+        choice.close();
+        return menuChoice;
+    }
+    public boolean seConnecter(){
+        Scanner input = new Scanner(System.in);
+        String username = "", password = "";
+        System.out.println("+----------------------------+");
+        System.out.println("|         CONNECTION         |");
+        System.out.println("|    Travaux de Montreal     |");
+        System.out.println("+----------------------------+");
+        System.out.print("Username: ");
+        username = input.nextLine();
+        System.out.print("Password: ");
+        password = input.nextLine();
+        return true;
+
+    }
+
 }
